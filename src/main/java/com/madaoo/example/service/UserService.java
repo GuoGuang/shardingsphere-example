49cf8697b58a9cbf4e6197384868670d6af88a94
lgg <linguoguang@inca.com.cn>
2021-06-25 15:50:07 +0800

    :white_check_mark: Adding tests.

--- src/main/java/com/madaoo/example/service/UserService.java
+++ src/main/java/com/madaoo/example/service/UserService.java
@@ -1,11 +1,13 @@
 package com.madaoo.example.service;
 
-
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.madaoo.example.mapper.UserMapper;
 import com.madaoo.example.pojo.User;
+import org.apache.shardingsphere.transaction.annotation.ShardingTransactionType;
+import org.apache.shardingsphere.transaction.core.TransactionType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService extends ServiceImpl<UserMapper, User> {
@@ -13,13 +15,17 @@ public class UserService extends ServiceImpl<UserMapper, User> {
     @Autowired
     UserMapper userMapper;
 
-//    @DS("gits_sharding")
-    public void insertSlave(User user) {
+    public void insertMaster(User user) {
         userMapper.insert(user);
     }
 
-//    @DS("gits_sharding")
-    public void insertMaster(User user) {
+    @ShardingTransactionType(TransactionType.XA)
+    @Transactional
+    public void insertTransactionSuccessful(User user) {
+        userMapper.insert(user);
+    }
+
+    public void insertTransactionFailed(User user) {
         userMapper.insert(user);
     }
 }
--- src/test/java/com/madaoo/example/SampleTest.java
+++ src/test/java/com/madaoo/example/SampleTest.java
@@ -6,6 +6,7 @@ import com.madaoo.example.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
@@ -18,29 +19,73 @@ public class SampleTest {
     @Resource
     private TestService testService;
 
+    @Resource
+    private UserService userService;
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+
     @Test
     public void testSave() throws Exception {
         for (long i = 0; i < 10; i++) {
             com.madaoo.example.pojo.Test test = new com.madaoo.example.pojo.Test();
             test.setId(i);
-            test.setText(i+"");
+            test.setText(i + "");
             test.setDate(new Date());
             testService.save(test);
         }
     }
 
-    @Resource
-    private UserService userService;
-
     @Test
     public void insert_test() throws Exception {
         for (long i = 0; i < 10; i++) {
-            User user1 = new User();
-            user1.setId(i);
-            user1.setName("1");
-            user1.setAge(i);
-            user1.setDate(new Date());
-            userService.insertMaster(user1);
+            User user = new User();
+            user.setId(i);
+            user.setName("1");
+            user.setAge(i);
+            user.setDate(new Date());
+            userService.insertMaster(user);
         }
+    }
+
+    /**
+     * 事务生效，数据回滚
+     *
+     * @throws Exception
+     */
+    @Test
+    public void xa_transaction_successful() throws Exception {
+        User user = new User();
+        user.setId(2l);
+        user.setName("2");
+        user.setDate(new Date());
+        userService.insertTransactionSuccessful(user);
+
+        User user1 = new User();
+        user1.setId(3l);
+        user1.setName("3");
+        user1.setDate(new Date());
+        userService.insertTransactionSuccessful(user1);
+        throw new RuntimeException();
+    }
+
+    /**
+     * 事务未生效，数据依旧插入
+     *
+     * @throws Exception
+     */
+    @Test
+    public void xa_transaction_failed() throws Exception {
+        User user = new User();
+        user.setId(2l);
+        user.setName("2");
+        user.setDate(new Date());
+        userService.insertTransactionFailed(user);
+
+        User user1 = new User();
+        user1.setId(3l);
+        user1.setName("3");
+        user1.setDate(new Date());
+        userService.insertTransactionFailed(user1);
+        throw new RuntimeException();
     }
 }
